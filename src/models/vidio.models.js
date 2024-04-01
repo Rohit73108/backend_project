7052e0b2f2d757850d666e7ecd72e3178e3c270d
github_Rohit73108 <rohitrana73108@gmail.com>
2024-04-02 00:24:59 +0530

    add user and video models

---
+++ src/models/vidio.models.js
@@ -0,0 +1,97 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const vidioSchema= new Schema(
+    {
+        vidioFile:{
+            type:String, // cloudinary url
+            required:true
+        },
+        thumbnail:{
+            type:String, // cloudinary url
+            required:true
+        },
+        title:{
+            type:String, 
+            required:true
+        },
+        description:{
+            type:String, 
+            required:true
+        },
+        duration:{
+            type:Number, 
+            required:true
+        },
+        views:{
+            type:Number, 
+            default: 0
+        },
+        isPublished:{
+            type:Boolean,
+            default:true
+        },
+        owner:{
+            type: Schema.Types.ObjectId,
+            ref:"User"
+        },
+
+    }
+    ,{timestamps:true})
+
+vidioSchema.plugin(mongooseAggregatePaginate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const Vidio= mongoose.model("Vidio",vidioSchema);
