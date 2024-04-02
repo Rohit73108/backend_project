0ea8a8f9bac9e7ee5dc4e29d7e0c69ff1ce8813e
github_Rohit73108 <rohitrana73108@gmail.com>
2024-04-03 00:04:55 +0530

    setting controlls and routes

--- src/app.js
+++ src/app.js
@@ -23,4 +23,17 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+
+// routes import usually import like this in between of the code file
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration 
+app.use("/api/v1/users",userRouter)
+
+
+// http://localhost8000/api/v1/users/register/login    this type of router url will be build
+
+
+
 export{app};
